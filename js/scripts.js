fe01f954745f8a870a26c3c5f1c78248669e5ef7
Ellie Angell and Frankie Gavin <ellie@>
2016-10-25 16:54:07 -0700

    alert with results

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   $("form#fortune_teller").submit(function(event) {
 
   $("#good-responses").show();
-
+  var userResponses=0;
   $("input:checkbox[name=good-fortune]:checked").each(function() {
     var goodLuck = $(this).val();
+    // userResponses.push(goodLuck);
     $("#good-responses").append(goodLuck + "<br>");
   });
 
@@ -12,8 +13,20 @@ $(document).ready(function(){
   $("input:checkbox[name=bad-fortune]:checked").each(function() {
     var badLuck = $(this).val();
     $("#bad-responses").append(badLuck + "<br>");
+    userResponses+=1;
   });
+  console.log(userResponses);
+   if (userResponses > 2) {
+     alert("yay!");
+   }
   $("#fortune_teller").hide();
   event.preventDefault();
   });
 });
+
+ // if (userResponses > 4) {
+ //   goodLuck = "yay!";
+ // }
+ // if else {
+ //   $("#bad-responses").text(badLuck)
+ // }
